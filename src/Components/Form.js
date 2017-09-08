3dc91348e9697c870b5f75a9d144cae868ef37ed
arorayash <arorayash.thisisus@gmail.com>
2017-09-08 08:41:50 +0530

    added warning-message component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,13 @@
 import React from "react";
+
 import Plot from "./Plot.js"
 import "./Form.css";
+
+function Tooltip(props) {
+    return (
+        <span className="warning-message">{props.text}</span>
+    )
+}
 
 export default class Form extends React.Component{
     constructor(props){
@@ -9,30 +16,48 @@ export default class Form extends React.Component{
         this.state = {
             name: "",
             value: "0",
+            valueWarn: false,
             startDate: null,
-            endDate: null
+            endDate: null,
+            dateWarn: false
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleValueChange = this.handleValueChange.bind(this);
+
+        this._onFocus = this._onFocus.bind(this);
+        this._onBlur = this._onBlur.bind(this);
+
+        this.showValueErrors = this.showValueErrors.bind(this);
+        this.showDateErrors = this.showDateErrors.bind(this);
     }
 
-    handleValueChange(){
-        //event.preventDefault();
-        if(this.refs.valueField.value>=0 && this.refs.valueField.value<=100)
-            this.setState({value: this.refs.valueField.value})
+    handleNameChange(e){
+        this.setState({name: e.target.value});
+    }
+
+    handleValueChange(e){
+        //console.dir(e.target.value);
+        if(e.target.value>=0 && e.target.value<=100)
+        {
+            this.setState({value: e.target.value, valueWarn:false});
+        }
         else
-            alert("Wrong val");
-
+        {
+            this.setState({value: "0", valueWarn:true});
+        }
     }
 
-    handleNameChange(){
-
-    }
-
-    handleSubmit(){
-
+    showValueErrors(){
+        let Warn;
+        if(this.state.valueWarn === true)
+        {
+            Warn = <Tooltip text="Incorrect Value"/>
+        } else {
+            Warn = null;
+        }
+        return Warn;
     }
 
     _onFocus(e){
@@ -40,29 +65,70 @@ export default class Form extends React.Component{
     }
 
     _onBlur(e){
-        //console.log(e.currentTarget.placeholder);
+        if(e.target.name === "startDateField")
+            this.setState({
+                startDate: e.target.value
+            });
+
+        let date = new Date(e.target.value);
+        console.log(date.getTime());
+        if(e.target.name === "endDateField" && this.state.startDate !== null)
+            alert("end date");
+
         e.currentTarget.type = "text";
     }
 
+    showDateErrors(){
+
+    }
+
+    handleSubmit(){
+        alert("No Errors!");
+    }
+
     render() {
+        let valueWarn = this.showValueErrors();
+        let dateWarn = this.showDateErrors();
         return (
             <div className="form-wrapper">
                 <form onSubmit={this.handleSubmit}>
                     <div className="input-group">
-                        <input className="name" onChange={this.handleNameChange} placeholder="Name" type="text"/>
+                        <input className="name"
+                               onBlur={this.handleNameChange}
+                               placeholder="Name"
+                               type="text"/>
                     </div>
                     <div className="input-group">
-                        <input className="value" onChange={this.handleValueChange} placeholder="Value(1-100)" ref="valueField" type="number"/>
+                        <input className="value"
+                               onChange={this.handleValueChange}
+                               placeholder="Value(1-100)"
+                               min={0}
+                               max={100}
+                               ref="valueField"
+                               type="number"
+                               data-tip='custom show'/>
+                        {valueWarn}
                     </div>
-
                     <div className="input-group">
-                        <input className="date start-date" onChange={this.handleChange} placeholder="Start Date" ref="startDateField" type="text" onFocus = {this._onFocus} onBlur={this._onBlur}/>
+                        <input className="date start-date"
+                               onChange={this.handleChange}
+                               placeholder="Start Date"
+                               name="startDateField"
+                               ref="startDateField"
+                               onFocus = {this._onFocus}
+                               onBlur={this._onBlur}/>
                     </div>
                     <div className="input-group">
-                        <input className="date end-date" onChange={this.handleChange} placeholder="End Date" ref="endDateField" type="text" onFocus = {this._onFocus} onBlur={this._onBlur}/>
+                        <input className="date end-date"
+                               onChange={this.handleChange}
+                               placeholder="End Date"
+                               name="endDateField"
+                               ref="endDateField"
+                               onFocus = {this._onFocus}
+                               onBlur={this._onBlur}/>
+                        {dateWarn}
                     </div>
                     <br/>
-
                     <Plot percentageValue={this.state.value}/>
                     <div className="submit input-group">
                         <input className="button" type="submit"/>
